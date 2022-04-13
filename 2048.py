45b982f6738b1b6e30772faf4d63b5b07bec2829
Yohan Guého <yohan.gueho@icloud.com>
2022-04-13 15:47:36 +0200

    Création fenêtre

--- 2048.py
+++ 2048.py
@@ -1,2 +1,18 @@
 # Projet
 # Auteurs : TROADEC-SAMOUR Mariam, TAIBI Yanis, GUEHO Yohan
+
+# Importation des modules
+
+import tkinter as tk
+import random
+
+# Constantes et variables
+
+LARGEUR = 700
+HAUTEUR = 700
+
+# Fonctions
+
+# Programme principal
+
+root = tk.Tk()
