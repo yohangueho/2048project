399c430e15105e8fe3b36427c63d82994f95eb69
Yohan Guého <yohan.gueho@icloud.com>
2022-05-02 14:37:23 +0200

    boutons

--- 2048.py
+++ 2048.py
@@ -5,11 +5,13 @@
 
 import tkinter as tk
 import random
+from turtle import right
 
 # Constantes et variables
 
 LARGEUR = 500
-HAUTEUR = 800
+HAUTEUR = 500
+color_0 = "white"
 
 matrice = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 case_1_1 = random.randint(0,3)
@@ -27,6 +29,18 @@ print(matrice)
 
 # Fonctions
 
+def start():
+    return
+
+def stop():
+    return
+
+def load():
+    return
+
+def save():
+    return
+
 def verify():
     return
 
@@ -36,38 +50,59 @@ def verify():
 
 root = tk.Tk()
 mainCanvas = tk.Canvas(root, bg="black", width=LARGEUR, height=HAUTEUR)
-gameArea = mainCanvas.create_rectangle(LARGEUR-480, HAUTEUR-780, LARGEUR-15, HAUTEUR-315, fill="red")
+gameArea = mainCanvas.create_rectangle(LARGEUR-480, HAUTEUR-480, LARGEUR-15, HAUTEUR-15, fill="red")
+
+start_button = tk.Button(root, text="START", height=2, width=20,command=start)
+stop_button = tk.Button(root, text="STOP", height=2, width=20, command=start)
+load_button = tk.Button(root, text="LOAD", height=2, width=20, command=start)
+save_button = tk.Button(root, text="SAVE", height=2, width=20, command=start)
+
+left_button = tk.Button(root, text="LEFT", height=5, width=10, command=start)
+up_button = tk.Button(root, text="UP", height=2, width=20, command=start)
+down_button = tk.Button(root, text="DOWN", height=2, width=20, command=start)
+right_button = tk.Button(root, text="RIGHT", height=5, width=10, command=start)
 
 # Création des cases (CASE_LIGNE_COLONNE)
 
 #Ligne 1
 
-CASE_1_1 = gameArea = mainCanvas.create_rectangle(LARGEUR-475, HAUTEUR-775, LARGEUR-365, HAUTEUR-665, fill="blue")
-CASE_1_2 = gameArea = mainCanvas.create_rectangle(LARGEUR-360, HAUTEUR-775, LARGEUR-250, HAUTEUR-665, fill="blue")
-CASE_1_3 = gameArea = mainCanvas.create_rectangle(LARGEUR-245, HAUTEUR-775, LARGEUR-135, HAUTEUR-665, fill="blue")
-CASE_1_4 = gameArea = mainCanvas.create_rectangle(LARGEUR-130, HAUTEUR-775, LARGEUR-20, HAUTEUR-665, fill="blue")
+CASE_1_1 = gameArea = mainCanvas.create_rectangle(LARGEUR-475, HAUTEUR-475, LARGEUR-365, HAUTEUR-365, fill=color_0)
+CASE_1_2 = gameArea = mainCanvas.create_rectangle(LARGEUR-360, HAUTEUR-475, LARGEUR-250, HAUTEUR-365, fill=color_0)
+CASE_1_3 = gameArea = mainCanvas.create_rectangle(LARGEUR-245, HAUTEUR-475, LARGEUR-135, HAUTEUR-365, fill=color_0)
+CASE_1_4 = gameArea = mainCanvas.create_rectangle(LARGEUR-130, HAUTEUR-475, LARGEUR-20, HAUTEUR-365, fill=color_0)
 
 #Ligne 2
 
-CASE_2_1 = gameArea = mainCanvas.create_rectangle(LARGEUR-475, HAUTEUR-660, LARGEUR-365, HAUTEUR-550, fill="blue")
-CASE_2_2 = gameArea = mainCanvas.create_rectangle(LARGEUR-360, HAUTEUR-660, LARGEUR-250, HAUTEUR-550, fill="blue")
-CASE_2_3 = gameArea = mainCanvas.create_rectangle(LARGEUR-245, HAUTEUR-660, LARGEUR-135, HAUTEUR-550, fill="blue")
-CASE_2_4 = gameArea = mainCanvas.create_rectangle(LARGEUR-130, HAUTEUR-660, LARGEUR-20, HAUTEUR-550, fill="blue")
+CASE_2_1 = gameArea = mainCanvas.create_rectangle(LARGEUR-475, HAUTEUR-360, LARGEUR-365, HAUTEUR-250, fill=color_0)
+CASE_2_2 = gameArea = mainCanvas.create_rectangle(LARGEUR-360, HAUTEUR-360, LARGEUR-250, HAUTEUR-250, fill=color_0)
+CASE_2_3 = gameArea = mainCanvas.create_rectangle(LARGEUR-245, HAUTEUR-360, LARGEUR-135, HAUTEUR-250, fill=color_0)
+CASE_2_4 = gameArea = mainCanvas.create_rectangle(LARGEUR-130, HAUTEUR-360, LARGEUR-20, HAUTEUR-250, fill=color_0)
 
 #Ligne 3
 
-CASE_3_1 = gameArea = mainCanvas.create_rectangle(LARGEUR-475, HAUTEUR-545, LARGEUR-365, HAUTEUR-435, fill="blue")
-CASE_3_2 = gameArea = mainCanvas.create_rectangle(LARGEUR-360, HAUTEUR-545, LARGEUR-250, HAUTEUR-435, fill="blue")
-CASE_3_3 = gameArea = mainCanvas.create_rectangle(LARGEUR-245, HAUTEUR-545, LARGEUR-135, HAUTEUR-435, fill="blue")
-CASE_3_4 = gameArea = mainCanvas.create_rectangle(LARGEUR-130, HAUTEUR-545, LARGEUR-20, HAUTEUR-435, fill="blue")
+CASE_3_1 = gameArea = mainCanvas.create_rectangle(LARGEUR-475, HAUTEUR-245, LARGEUR-365, HAUTEUR-135, fill=color_0)
+CASE_3_2 = gameArea = mainCanvas.create_rectangle(LARGEUR-360, HAUTEUR-245, LARGEUR-250, HAUTEUR-135, fill=color_0)
+CASE_3_3 = gameArea = mainCanvas.create_rectangle(LARGEUR-245, HAUTEUR-245, LARGEUR-135, HAUTEUR-135, fill=color_0)
+CASE_3_4 = gameArea = mainCanvas.create_rectangle(LARGEUR-130, HAUTEUR-245, LARGEUR-20, HAUTEUR-135, fill=color_0)
 
 #Ligne 4
 
-CASE_4_1 = gameArea = mainCanvas.create_rectangle(LARGEUR-475, HAUTEUR-430, LARGEUR-365, HAUTEUR-320, fill="blue")
-CASE_4_2 = gameArea = mainCanvas.create_rectangle(LARGEUR-360, HAUTEUR-430, LARGEUR-250, HAUTEUR-320, fill="blue")
-CASE_4_3 = gameArea = mainCanvas.create_rectangle(LARGEUR-245, HAUTEUR-430, LARGEUR-135, HAUTEUR-320, fill="blue")
-CASE_4_4 = gameArea = mainCanvas.create_rectangle(LARGEUR-130, HAUTEUR-430, LARGEUR-20, HAUTEUR-320, fill="blue")
+CASE_4_1 = gameArea = mainCanvas.create_rectangle(LARGEUR-475, HAUTEUR-130, LARGEUR-365, HAUTEUR-20, fill=color_0)
+CASE_4_2 = gameArea = mainCanvas.create_rectangle(LARGEUR-360, HAUTEUR-130, LARGEUR-250, HAUTEUR-20, fill=color_0)
+CASE_4_3 = gameArea = mainCanvas.create_rectangle(LARGEUR-245, HAUTEUR-130, LARGEUR-135, HAUTEUR-20, fill=color_0)
+CASE_4_4 = gameArea = mainCanvas.create_rectangle(LARGEUR-130, HAUTEUR-130, LARGEUR-20, HAUTEUR-20, fill=color_0)
 
 
-mainCanvas.grid()
+mainCanvas.grid(column=0, columnspan=4, row=0)
+
+start_button.grid(column=0, row=1, columnspan=2)
+stop_button.grid(column=2, row=1, columnspan=2)
+load_button.grid(column=0, row=2, columnspan=2)
+save_button.grid(column=2, row=2, columnspan=2)
+
+left_button.grid(column=0, row=3, rowspan=2)
+up_button.grid(column=1, row=3, columnspan=2)
+down_button.grid(column=1, row=4, columnspan=2)
+right_button.grid(column=3, row=3, rowspan=2)
+
 root.mainloop()
